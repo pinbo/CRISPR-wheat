6f0e10a06a09b36309f7e650f2d8ea54dbd891e7
Junli Zhang <zhjl86@gmail.com>
2018-06-03 19:08:12 -0700

    First working script

---
+++ bin/functions.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  functions.py
+#  
+#  Copyright 2018 Junli Zhang <zhjl86@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+import getopt, sys, os, re, copy
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
+
+##################### CLASSES   ######################
+# guide RNA object
+class gRNA(object):
+	"""Information of guide RNA"""
+	def __init__(self):
+		self.name = ""
+		self.start = 0 # 0 based
+		self.end = 0 # 0 based
+		self.length = 0
+		self.gc = 0.0
+		self.seq = "" # sequence without PAM
+		self.pam = "" # PAM
+		self.direction = ""
+		self.template = ""
+		self.REs = [] # list of restriction enzymes
+
+class Restriction_Enzyme(object):
+	def __init__(self, name, seq):
+		self.name = name
+		self.seq = seq.lower()
+		self.length = len(seq)
+		self.template_seq = ""
+		self.caps = "No"
+		self.allpos = [] # all the match positions in the template
+		self.price = int(name.split(',')[-1])
+		self.primer_direction = "" # to use the end positions as left or right primer
+
+
+##################### FUNCTIONS ######################
+# function to extract sequences from a fasta file 
+def get_fasta(infile):
+	fasta = {} # dictionary for alignment
+	with open(infile) as file_one:
+		for line in file_one:
+			line = line.strip()
+			if line.startswith(">"):
+				sequence_name = line.split()[0].lstrip(">")
+			else:
+				fasta.setdefault(sequence_name, "")
+				fasta[sequence_name] += line.strip().replace(" ", "") # remove spaces in case
+	return fasta
+
+# get reverse complement sequence
+def ReverseComplement(seq):
+	s1 = "BDHKMNRSVWYATGCbdhkmnrsvwyatgc"
+	s2 = "VHDMKNYSBWRTACGvhdmknysbwrtacg"
+	seq_dict = {s1[i]:s2[i] for i in range(len(s1))}
+	return "".join([seq_dict[base] for base in reversed(seq)])
+
+#from sys import platform
+def get_software_path(base_path):
+	if sys.platform.startswith('linux'): # linux
+		primer3_path = base_path + "/primer3_core"
+		muscle_path = base_path + "/muscle"
+	elif sys.platform == "win32" or sys.platform == "cygwin": # Windows...
+		primer3_path = base_path + "/primer3_core.exe"
+		muscle_path = base_path + "/muscle.exe"
+	elif sys.platform == "darwin": # MacOSX
+		primer3_path = base_path + "/primer3_core_darwin64"
+		muscle_path = base_path + "/muscle3.8.31_i86darwin64"
+	return primer3_path, muscle_path
+
+# parse the restriciton enyzme file
+def parse_RE_file(RE_file):
+	REs = {}
+	with open(RE_file) as file_one:
+		for line in file_one:
+			enzyme, seq = line.rstrip().split("\t")
+			REs[enzyme] = Restriction_Enzyme(enzyme, seq.strip("N")) # remove leading and ending Ns
+	return REs
+
+def find_substring(substring, string): # find all the starting index of a substring
+	return [m.start() for m in re.finditer(substring, string)]
+
+def seq2pattern(seq):
+	iupac = {
+		"B": "[CGT]",
+		"D": "[AGT]",
+		"H": "[ACT]",
+		"K": "[GT]",
+		"M": "[AC]",
+		"N": "[ACGT]",
+		"R": "[AG]",
+		"S": "[CG]",
+		"V": "[ACG]",
+		"W": "[AT]",
+		"Y": "[CT]"
+	}
+	seq = seq.upper()
+	seq2 = ""
+	for i in seq:
+		if i in "ATGC":
+			seq2 += i
+		else:
+			seq2 += iupac[i]
+	return seq2.lower()
+
+# test whether an enzyme can be modifed to fit a dCAPS
+def test_enzyme(enzyme_class, seq): # enzyme is an Restriction_Enzyme object
+	enzyme = copy.copy(enzyme_class)
+	enzyme_seq = enzyme.seq
+	enzyme_seq_RC = ReverseComplement(enzyme_seq) # 1
+	enzyme_name = enzyme.name
+	wild_seq = seq.lower()
+	wild_allpos = find_substring(seq2pattern(enzyme_seq), wild_seq)
+	wild_allpos += find_substring(seq2pattern(enzyme_seq_RC), wild_seq) # also check reverse complement sequences of enzyme
+	enzyme.allpos = list(set(wild_allpos))
+	if enzyme.allpos:
+		enzyme.caps = "Yes"
+	return enzyme
+
+# calculate GC content of a sequence
+def Calc_GC(seq):
+	t = 0.0 # float
+	for a in seq:
+		if a in 'GCgc':
+			t += 1
+	return t / len(seq) * 100
+
+# find PAM positions
+def find_pam(seq, pam, grna_length, direction):
+	allpos = find_substring(seq2pattern(pam), seq)
+	grna_list = []
+	for i in allpos:
+		grna = gRNA()
+		grna.direction = direction
+		grna.length = grna_length
+		grna.start = i - grna_length
+		grna.end = i - 1
+		if grna.start < 0:
+			continue
+		grna.seq = seq[grna.start:i].upper()
+		grna.gc = Calc_GC(grna.seq)
+		grna.pam = seq[i:(i+len(pam))].upper()
+		grna_list.append(grna)
+	return grna_list
+
+# test specificity of a gRNA
+def test_spec(seq, targets, non_targets, fasta_raw):
+	seq = seq.lower()
+	spec = 1 # whether specific
+	# seq should be in all targets
+	for j in targets:
+		tt = fasta_raw[j].lower()
+		#if seq not in tt:
+		if not find_substring(seq2pattern(seq), tt):
+			spec = 0
+			break
+	if not spec:
+		return spec
+	# seq should NOT be in any targets
+	# for non-target, I think I only need to test the last 12 bp
+	# with confirm with Yanpeng
+	for k in non_targets:
+		tt2 = fasta_raw[k].lower()
+		#if seq in tt2:
+		if find_substring(seq2pattern(seq), tt2):
+			spec = 0
+			break
+	return spec
+
---
+++ bin/get_guide_RNA.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  get_guide_RNA.py
+#  
+#  Copyright 2018 Junli Zhang <zhjl86@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+### Imported
+#from subprocess import call
+import getopt, sys, os, re
+from functions import *
+
+
+
+############
+usage="""
+get_guide_RNA.py
+	-i <sequence.fa>
+	-t <seqID1,seqID2>
+	-o <output file name>
+	-b <blast all produced primers against the genomes: 1 for YES and 0 for NO, default is NO>
+	-l <sgRNA length, default is 20>
+	-a <whether to target all homeologs: 1 for YES and 0 for NO. default is 1>
+	-p <PAM sequence. default is NGG>
+	-h <print the help>
+"""
+
+# parameters
+pam = "NGG"
+grna_length = 20
+target_all = 1 # whether to design sg-RNA for all targets
+seqfile = "sequence.fa" # input is a fasta file with homeologs
+targets = [] # target sequence names
+out = "" # output file
+mainID = "" # the one used as template to search sgRNAs
+max_price = 200 # maximum restriciton enzyme price
+# steps
+
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "i:p:s:l:g:r:o:m:v:f:a:e:c:b:d:h", ["help"])
+except getopt.GetoptError as err:
+	# print help information and exit:
+	print str(err)  # will print something like "option -a not recognized"
+	print usage
+	sys.exit(2)
+for o, a in opts:
+	if o == "-i":
+		seqfile = a
+	elif o in ("-h", "--help"):
+		print usage
+		sys.exit()
+	elif o in ("-a"):
+		target_all = int(a)
+	elif o in ("-t"):
+		targets = a.split(",")
+		target_all = 0
+		print targets
+	elif o in ("-o"):
+		out = a
+	elif o in ("-l"):
+		grna_length = int(a)
+	elif o in ("-p"):
+		pam = a
+	elif o in ("-b"):
+		blast = int(a)
+	else:
+		assert False, "unhandled option"
+print "Options done"
+
+## read fasta
+# get target and ids and rename fasta seq names
+fasta_raw = get_fasta(seqfile)
+fasta_raw_RC = get_fasta(seqfile)
+for k in fasta_raw_RC:
+	fasta_raw_RC[k] = ReverseComplement(fasta_raw_RC[k])
+
+seq_names = fasta_raw.keys()
+if target_all == 1:
+	targets = seq_names
+
+mainID = targets[0]
+non_targets = [i for i in seq_names if i not in targets] # other non-target sequence names
+print "non_targets are ", non_targets
+
+wild_seq = fasta_raw[mainID]
+wild_seq_RC = ReverseComplement(wild_seq)
+groupname = "-".join(targets)
+if not out:
+	out = 'selected_gRNAs_for_' + groupname + ".txt"
+
+getcaps_path = os.path.dirname(os.path.realpath(__file__))
+
+
+# software path
+#primer3_path, muscle_path = get_software_path(getcaps_path)
+
+
+## Get restriciton enzyme information
+# step 1: read the enzyme file
+RE_file = getcaps_path + "/NEB_parsed_REs.txt" # this one removed some duplicated cuttings
+REs = parse_RE_file(RE_file) # get the list of restriction enzymes
+# step 2: get the list of enzymes that can be used
+caps_list_forward = []
+caps_list_reverse = []
+for k in REs:
+	enzyme = REs[k]
+	if enzyme.price > max_price:
+		continue
+	enzyme1 = test_enzyme(enzyme, wild_seq)
+	enzyme2 = test_enzyme(enzyme, wild_seq_RC)
+	if enzyme1.caps == "Yes":
+		caps_list_forward.append(enzyme1)
+	if enzyme2.caps == "Yes":
+		caps_list_reverse.append(enzyme2)
+
+#print "RE list is ", caps_list_forward + caps_list_reverse
+
+## find all potential gRNAs in the sequences and RC sequence
+forward_grnas = find_pam(wild_seq, pam, grna_length, "Forward")
+reverse_grnas = find_pam(wild_seq_RC, pam, grna_length, "Reverse")
+
+print "RAW forward gRNA number ", len(forward_grnas)
+print "RAW reverse gRNA number ", len(reverse_grnas)
+
+## filter gRNAs based on specificity
+specific_forward_grnas = []
+specific_reverse_grnas = []
+
+for i in forward_grnas:
+	seq = i.seq + i.pam
+	if test_spec(seq, targets, non_targets, fasta_raw):
+		specific_forward_grnas.append(i)
+
+for i in reverse_grnas:
+	seq = i.seq + i.pam
+	if test_spec(seq, targets, non_targets, fasta_raw_RC):
+		specific_reverse_grnas.append(i)
+
+print "Specific forward gRNA number ", len(specific_forward_grnas)
+print "Specific reverse gRNA number ", len(specific_reverse_grnas)
+
+## find whether there are restrction enzymes (REs) on the selected sequences with PAM
+## only REs with recognization site overlap with the 4th position from the end of sg-RNA
+for i in specific_forward_grnas:
+	cut_pos = i.end - 3
+	#print "cut pos is ", cut_pos, i.seq
+	for j in caps_list_forward:
+		allpos = j.allpos
+		for k in allpos:
+			rr = range(k, k + j.length)
+			if cut_pos in rr:
+				#print j.name, j.seq, j.allpos
+				i.REs.append(j.name + "," + j.seq)
+
+for i in specific_reverse_grnas:
+	cut_pos = i.end - 3
+	for j in caps_list_reverse:
+		allpos = j.allpos
+		for k in allpos:
+			rr = range(k, k + j.length)
+			if cut_pos in rr:
+				i.REs.append(j.name + "," + j.seq)
+
+
+## Print output files
+
+# write to file
+outfile = open(out, 'w')
+outfile.write("Start\tEnd\tStrand\tLength\tSequence\tGC_content\tReverseComplement\tPAM\tTemplate\tRestriction_Enzyme\n")
+template_length = len(wild_seq)
+for i in specific_forward_grnas:
+	outfile.write("\t".join([str(i.start + 1), str(i.end + 1), i.direction, str(i.length), i.seq, str(i.gc), ReverseComplement(i.seq), i.pam, mainID, ";".join(i.REs)]) + "\n")
+
+for i in specific_reverse_grnas:
+	outfile.write("\t".join([str(template_length - i.start), str(template_length - i.end), i.direction, str(i.length), i.seq, str(i.gc), ReverseComplement(i.seq), i.pam, mainID, ";".join(i.REs)]) + "\n")
+
+
+
